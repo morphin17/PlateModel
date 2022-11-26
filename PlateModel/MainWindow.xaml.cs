450c3d5d3f554fcf9c5b519e089a2f72f83acfc1
Rakipov Dinaf <rakipov69@gmail.com>
2022-11-26 23:36:58 +0300

    first version

--- PlateModel/MainWindow.xaml.cs
+++ PlateModel/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace PlateModel
 {
@@ -20,9 +21,392 @@ namespace PlateModel
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        /** Анимация **/
+        private bool animateFlag = false;       //Воспроизвести
+        DispatcherTimer timer;                  //Таймер для анимации
+
+        double[] substance = { 0.000127, 0.00016563, 0.000111, 0.000088 };
+
+        public Plate p1;                         //Пластина, числнное решение
+        public Plate p2;                        //Пластина, для шага в два раза больше
+
+        NCalc.Expression exprF1;
+        NCalc.Expression exprPhi1;
+        NCalc.Expression exprMu1;
+        NCalc.Expression exprU1;
+
+        NCalc.Expression exprF2;
+        NCalc.Expression exprPhi2;
+        NCalc.Expression exprMu2;
+        NCalc.Expression exprU2;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            timer = new DispatcherTimer();
+
+            timer.Tick += new EventHandler(timer_Tick);
+
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 40);
+
+            timer.Start();
+
+
+            init_PlateOptions1();
+            init_Expr1();
+            init_Plate1();
+            init_HeatMap1();
+
+            init_PlateOptions2();
+            init_Expr2();
+            init_Plate2();
+            init_HeatMap2();
         }
+
+        private void MenuItem_Test1(object sender, RoutedEventArgs e)
+        {
+            init_PlateOptions1();
+            init_PlateOptions2();
+
+            PlateFunctionF1.Text = "0";
+            PlateFunctionPhi1.Text = "Sin(pi*x/5)*Sin(pi*y/5)";
+            PlateFunctionMu1.Text = "0";
+            PlateWidth1.Value = 5;
+            PlateWidthPointsCount1.Value = 20;
+            PlateHeight1.Value = 5;
+            PlateHeightPointsCount1.Value = 20;
+            PlateTimeStep1.Value = 500;
+            PlateFunctionU1.Text = "Sin(pi*x/5)*Sin(pi*y/5)*Pow(e, -0.000127*(pi*pi/(5*5) + pi*pi/(5*5))*t)";
+            PlateSubstance1.SelectedIndex = 0;
+            BottomBorderTemp1.Value = 0;
+            TopBorderTemp1.Value = 1;
+
+            PlateFunctionF2.Text = "0";
+            PlateFunctionPhi2.Text = "Sin(pi*x/5)*Sin(pi*y/5)";
+            PlateFunctionMu2.Text = "0";
+            PlateWidth2.Value = 5;
+            PlateWidthPointsCount2.Value = 20;
+            PlateHeight2.Value = 5;
+            PlateHeightPointsCount2.Value = 20;
+            PlateTimeStep2.Value = 500;
+            PlateSubstance2.SelectedIndex = 0;
+            BottomBorderTemp2.Value = 0;
+            TopBorderTemp2.Value = 1;
+
+            init_Expr1();
+            init_Expr2();
+
+            init_Plate1();
+            init_HeatMap1();
+            init_Plate2();
+            init_HeatMap2();
+        }
+
+        private void init_PlateOptions1()
+        {
+            PlateWidth1.Value = 10;
+            PlateWidthPointsCount1.Value = 20;
+            PlateHeight1.Value = 10;
+            PlateHeightPointsCount1.Value = 20;
+            PlateTimeStep1.Value = 0.5;
+            PlateFunctionF1.Text = "0";
+            PlateFunctionPhi1.Text = "0";
+            PlateFunctionMu1.Text = "0";
+            PlateFunctionU1.Text = "";
+            PlateSubstance1.SelectedIndex = 0;
+            BottomBorderTemp1.Value = 0;
+            TopBorderTemp1.Value = 10;
+        }
+
+        private void init_Expr1()
+        {
+            exprF1 = new NCalc.Expression(PlateFunctionF1.Text);
+            exprPhi1 = new NCalc.Expression(PlateFunctionPhi1.Text);
+            exprMu1 = new NCalc.Expression(PlateFunctionMu1.Text);
+            if (PlateFunctionU1.Text != "")
+            {
+                exprU1 = new NCalc.Expression(PlateFunctionU1.Text);
+            }
+        }
+        private void init_Expr2()
+        {
+            exprF2 = new NCalc.Expression(PlateFunctionF2.Text);
+            exprPhi2 = new NCalc.Expression(PlateFunctionPhi2.Text);
+            exprMu2 = new NCalc.Expression(PlateFunctionMu2.Text);
+            if (PlateFunctionU2.Text != "")
+            {
+                exprU2 = new NCalc.Expression(PlateFunctionU2.Text);
+            }
+        }
+
+        private void init_PlateOptions2()
+        {
+            PlateWidth2.Value = 10;
+            PlateWidthPointsCount2.Value = 20;
+            PlateHeight2.Value = 10;
+            PlateHeightPointsCount2.Value = 20;
+            PlateTimeStep2.Value = 0.5;
+            PlateFunctionF2.Text = "0";
+            PlateFunctionPhi2.Text = "0";
+            PlateFunctionMu2.Text = "0";
+            PlateFunctionU2.Text = "";
+            PlateSubstance2.SelectedIndex = 0;
+            BottomBorderTemp2.Value = 0;
+            TopBorderTemp2.Value = 10;
+        }
+
+
+        private void init_Plate1()
+        {
+            try
+            {
+                p1 = new Plate(
+                    substance[PlateSubstance1.SelectedIndex],
+                    Convert.ToDouble(PlateWidth1.Value),
+                    Convert.ToDouble(PlateHeight1.Value),
+                    Convert.ToDouble(PlateWidth1.Value) / Convert.ToDouble(PlateWidthPointsCount1.Value),
+                    Convert.ToDouble(PlateHeight1.Value) / Convert.ToDouble(PlateHeightPointsCount1.Value),
+                    Convert.ToDouble(PlateTimeStep1.Value),
+                    phi1,
+                    f1,
+                    mu1,
+                    u1
+                );
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка. Проверьте корректно ли вы ввели начальные условия для левой пластины.");
+                return;
+            }
+        }
+        private void init_Plate2()
+        {
+            try
+            {
+                p2 = new Plate(
+                    substance[PlateSubstance2.SelectedIndex],
+                    Convert.ToDouble(PlateWidth2.Value),
+                    Convert.ToDouble(PlateHeight2.Value),
+                    Convert.ToDouble(PlateWidth2.Value) / Convert.ToDouble(PlateWidthPointsCount2.Value),
+                    Convert.ToDouble(PlateHeight2.Value) / Convert.ToDouble(PlateHeightPointsCount2.Value),
+                    Convert.ToDouble(PlateTimeStep2.Value),
+                    phi2,
+                    f2,
+                    mu2,
+                    u2
+                );
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка. Проверьте корректно ли вы ввели начальные условия для левой пластины.");
+                return;
+            }
+        }
+
+        private void init_HeatMap1()
+        {
+
+            var plt = WpfPlotPlate1.Plot;
+
+            plt.YAxis.Ticks(false);
+            plt.YAxis.Grid(false);
+            plt.YAxis2.Ticks(true);
+            plt.YAxis2.Grid(true);
+
+
+            double[,] data2D;
+
+            if (PlateFunctionU1.Text == "")
+            {
+                data2D = p1.U;
+            }
+            else
+            {
+                data2D = p1.solutionU;
+            }
+
+
+            plt.Clear();
+
+            var hm = plt.AddHeatmap(data2D, lockScales: true);
+            hm.Update(data2D, min: BottomBorderTemp1.Value, max: TopBorderTemp1.Value);
+
+            hm.YAxisIndex = 1;
+            hm.OffsetX = 0;
+            hm.OffsetY = 0;
+            hm.CellWidth = Convert.ToDouble(PlateWidth1.Value) / Convert.ToDouble(PlateWidthPointsCount1.Value);
+            hm.CellHeight = Convert.ToDouble(PlateHeight1.Value) / Convert.ToDouble(PlateHeightPointsCount1.Value);
+            plt.Margins(0, 0);
+
+            WpfPlotPlate1.Refresh();
+        }
+        private void init_HeatMap2()
+        {
+
+            var plt = WpfPlotPlate2.Plot;
+
+
+            double[,] data2D;
+
+            if (PlateFunctionU2.Text == "")
+            {
+                data2D = p2.U;
+            }
+            else
+            {
+                data2D = p2.solutionU;
+            }
+
+            plt.Clear();
+            var hm = plt.AddHeatmap(data2D, lockScales: true);
+            hm.Update(data2D, min: BottomBorderTemp2.Value, max: TopBorderTemp2.Value);
+
+            hm.OffsetX = 0;
+            hm.OffsetY = 0;
+            hm.CellWidth = Convert.ToDouble(PlateWidth2.Value) / Convert.ToDouble(PlateWidthPointsCount2.Value);
+            hm.CellHeight = Convert.ToDouble(PlateHeight2.Value) / Convert.ToDouble(PlateHeightPointsCount2.Value);
+            plt.Margins(0, 0);
+
+            WpfPlotPlate2.Refresh();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+        }
+
+        private void next_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (PlateFunctionU1.Text == "") { 
+                    p1.nextU();
+                }
+                else
+                {
+                    p1.nextSolution();
+                }
+
+                if (PlateFunctionU2.Text == "") { 
+                    p2.nextU();
+                }
+                else
+                {
+                    p2.nextSolution();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка. Проверьте начальные условия");
+                return;
+            }
+
+            init_HeatMap1();
+            init_HeatMap2();
+
+
+            CurrentModelTime1.Text = "Текущее время:\n" + Convert.ToString(p1.curT);
+            CurrentModelTime2.Text = "Текущее время:\n" + Convert.ToString(p2.curT);
+        }
+
+
+        private double f1(double x, double y, double t)
+        {
+            exprF1.Parameters["pi"] = Math.PI;
+            exprF1.Parameters["e"] = Math.E;
+            exprF1.Parameters["x"] = x;
+            exprF1.Parameters["y"] = y;
+            exprF1.Parameters["t"] = t;
+
+            double r = Convert.ToDouble(exprF1.Evaluate());
+            return r;
+        }
+        private double f2(double x, double y, double t)
+        {
+            exprF2.Parameters["pi"] = Math.PI;
+            exprF2.Parameters["e"] = Math.E;
+            exprF2.Parameters["x"] = x;
+            exprF2.Parameters["y"] = y;
+            exprF2.Parameters["t"] = t;
+
+            double r = Convert.ToDouble(exprF2.Evaluate());
+            return r;
+        }
+
+        private double phi1(double x, double y)
+        {
+            exprPhi1.Parameters["pi"] = Math.PI;
+            exprPhi1.Parameters["e"] = Math.E;
+            exprPhi1.Parameters["x"] = x;
+            exprPhi1.Parameters["y"] = y;
+
+            double r = Convert.ToDouble(exprPhi1.Evaluate());
+            return r;
+        }
+        private double phi2(double x, double y)
+        {
+            exprPhi2.Parameters["pi"] = Math.PI;
+            exprPhi2.Parameters["e"] = Math.E;
+            exprPhi2.Parameters["x"] = x;
+            exprPhi2.Parameters["y"] = y;
+
+            double r = Convert.ToDouble(exprPhi2.Evaluate());
+            return r;
+        }
+
+        private double mu1(double t)
+        {
+            exprMu1.Parameters["pi"] = Math.PI;
+            exprMu1.Parameters["e"] = Math.E;
+            exprMu1.Parameters["t"] = t;
+
+            double r = Convert.ToDouble(exprMu1.Evaluate());
+            return r;
+        }
+
+        private double mu2(double t)
+        {
+            exprMu2.Parameters["pi"] = Math.PI;
+            exprMu2.Parameters["e"] = Math.E;
+            exprMu2.Parameters["t"] = t;
+
+            double r = Convert.ToDouble(exprMu2.Evaluate());
+            return r;
+        }
+
+        private double u1(double x, double y, double t)
+        {
+            if (PlateFunctionU1.Text == "")
+            {
+                return 0;
+            }
+            exprU1.Parameters["pi"] = Math.PI;
+            exprU1.Parameters["e"] = Math.E;
+            exprU1.Parameters["t"] = t;
+            exprU1.Parameters["x"] = x;
+            exprU1.Parameters["y"] = y;
+
+            double r = Convert.ToDouble(exprU1.Evaluate());
+            return r;
+        }
+        private double u2(double x, double y, double t)
+        {
+            if (PlateFunctionU2.Text == "")
+            {
+                return 0;
+            }
+
+            exprU2.Parameters["pi"] = Math.PI;
+            exprU2.Parameters["e"] = Math.E;
+            exprU2.Parameters["t"] = t;
+            exprU2.Parameters["x"] = x;
+            exprU2.Parameters["y"] = y;
+
+            double r = Convert.ToDouble(exprU2.Evaluate());
+            return r;
+        }
+
     }
 }
---
+++ PlateModel/Plate.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlateModel
+{
+    public class Plate
+    {
+
+        public double a;
+        public double b;
+
+        public double h1;
+        public double h2;
+
+        public double tau;
+        public double curT;
+
+        public double sigma1;
+        public double sigma2;
+        private double gamma1;
+        private double gamma2;
+        public double maxR = 0;
+
+
+        private Func<double, double> mu;
+        private Func<double, double, double> phi;
+        private Func<double, double, double, double> f;
+        private Func<double, double, double, double> u;
+        /// <summary>
+        /// Пластина в текущий момент времени
+        /// </summary>
+        public double[,] U;
+        public double[,] solutionU;
+        public double[,] FF;
+
+        /// <summary>
+        /// Инициализация входных параметров
+        /// </summary>
+        /// <param name="ro">Коэффициент температуропроводности</param>
+        /// <param name="a">Длина пластины по x</param>
+        /// <param name="b">Длина пластины по y</param>
+        /// <param name="h1">Шаг по x</param>
+        /// <param name="h2">Шаг по y</param>
+        /// <param name="tau">Шаг по времени</param>
+        /// <param name="phi">Функция при краевом условии t=0</param>
+        /// <param name="f">Функция нагрева</param>
+        /// <param name="mu">Функция при кравеаом условии на границах пластины</param>
+        /// <param name="u">Аналитическое решение</param>
+        public Plate(double ro, double a, double b, double h1, double h2, double tau,
+            Func<double, double, double> phi, Func<double, double, double, double> f, Func<double, double> mu, 
+            Func<double, double, double, double> u
+            )
+        {
+            this.a = a;
+            this.b = b;
+            this.h1 = h1;
+            this.h2 = h2;
+            this.tau = tau;
+
+            this.sigma1 = 0.5 * (ro - h1 * h1 / (6 * tau));
+            this.sigma2 = 0.5 * (ro - h2 * h2 / (6 * tau));
+            this.gamma1 = tau / (h1 * h1);
+            this.gamma2 = tau / (h2 * h2);
+
+            this.mu = mu;
+            this.phi = phi;
+            this.f = f;
+            this.u = u;
+
+            this.curT = 0;
+            this.U = new double[Convert.ToInt64(a / h1) + 1, Convert.ToInt64(b / h2) + 1];
+            this.solutionU = new double[Convert.ToInt64(a / h1) + 1, Convert.ToInt64(b / h2) + 1];
+            this.FF = new double[Convert.ToInt64(a / h1) + 1, Convert.ToInt64(b / h2) + 1];
+
+            //Пластина в нулевой момент времени
+            for (int i = 0; i < this.U.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.U.GetLength(1); j++)
+                {
+                    this.U[i, j] = phi(h1 * i, h2 * j);
+                }
+            }
+
+            //Краевые условия на краях пластины
+            this.conditionT();
+
+            this.calcSolution(0);
+        }
+
+        private void calcFF()
+        {
+            for (int i = 0; i < this.U.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.U.GetLength(1); j++)
+                {
+                    this.FF[i, j] = f(h1 * i, h2 * j, curT + tau / 2);
+                }
+            }
+        }
+
+
+        public void nextSolution()
+        {
+            curT += tau;
+            calcSolution(curT);
+        }
+
+        /// <summary>
+        /// Посчитать сетку для аналитического решения
+        /// </summary>
+        private void calcSolution(double curT)
+        {
+            for (int i = 0; i < this.U.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.U.GetLength(1); j++)
+                {
+                    this.solutionU[i, j] = u(h1 * i, h2 * j, curT);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Краевые условия на краях пластины
+        /// </summary>
+        private void conditionT()
+        {
+            for (int i = 0; i < U.GetLength(0); i++)
+            {
+                U[i, 0] = mu(curT);
+                U[i, U.GetLength(1) - 1] = mu(curT);
+            }
+
+            for (int j = 0; j < U.GetLength(1); j++)
+            {
+                U[0, j] = mu(curT);
+                U[U.GetLength(0) - 1, j] = mu(curT);
+            }
+        }
+
+        /// <summary>
+        /// Вспомогательная функция для подсчета схемы
+        /// </summary>
+        /// <param name="U">Пластина</param>
+        /// <param name="i">Координаты</param>
+        /// <param name="j">Координаты</param>
+        /// <returns></returns>
+        private double F(double[,] U, int i, int j)
+        {
+
+            return (1 - sigma1) * (U[i - 1, j] - 2 * U[i, j] + U[i + 1, j]) / (h1 * h1) +
+                (1 - sigma2) * (U[i, j - 1] - 2 * U[i, j] + U[i, j + 1]) / (h2 * h2) +
+                (1 - sigma1) * (1 - sigma2) * tau *
+                (U[i - 1, j - 1] - 2 * U[i - 1, j] + U[i - 1, j + 1] - 2 * U[i, j - 1] + 4 * U[i, j] - 2 * U[i, j + 1] + U[i + 1, j - 1] - 2 * U[i + 1, j] + U[i + 1, j + 1]) /
+                ((h1 * h1) * (h2 * h2)) +
+                ((double)1 / 12) * (FF[i - 1, j] - 4 * FF[i, j] + FF[i + 1, j] + FF[i, j - 1] + FF[i, j + 1]) + FF[i, j];
+        }
+
+        /// <summary>
+        /// Переход на следующий временной слой
+        /// </summary>
+        public void nextU()
+        {
+            calcFF();
+            Double[,] N;
+            double[] alpha;
+            double[] beta;
+
+            N = Copy(U);
+            alpha = new double[U.GetLength(0) + 1];
+            beta = new double[U.GetLength(0) + 1];
+
+            //Прогонка по x
+            for (int j = 1; j < U.GetLength(1) - 1; j++)
+            {
+                alpha[1] = 0;
+                beta[1] = U[0, j];
+
+                for (int i = 1; i < U.GetLength(0) - 1; i++)
+                {
+                    alpha[i + 1] = sigma1 * gamma1 / (1 + 2 * sigma1 * gamma1 - alpha[i] * sigma1 * gamma1);
+                    beta[i + 1] = (tau * F(N, i, j) + N[i, j] + sigma1 * gamma1 * beta[i]) / (1 + 2 * sigma1 * gamma1 - alpha[i] * sigma1 * gamma1);
+                }
+
+                for (int i = U.GetLength(0) - 2; i > 0; i--)
+                {
+                    U[i, j] = alpha[i + 1] * U[i + 1, j] + beta[i + 1];
+                }
+            }
+
+            N = Copy(U);
+            alpha = new double[U.GetLength(1) + 1];
+            beta = new double[U.GetLength(1) + 1];
+
+            //Прогонка по y
+            for (int i = 1; i < U.GetLength(0) - 1; i++)
+            {
+                alpha[1] = 0;
+                beta[1] = U[i, 0];
+
+                for (int j = 1; j < U.GetLength(1) - 1; j++)
+                {
+                    alpha[j + 1] = sigma2 * gamma2 / (1 + 2 * sigma2 * gamma2 - alpha[j] * sigma2 * gamma2);
+                    beta[j + 1] = (N[i, j] + sigma2 * gamma2 * beta[j]) / (1 + 2 * sigma2 * gamma2 - alpha[j] * sigma2 * gamma2);
+                }
+
+                for (int j = U.GetLength(1) - 2; j > 0; j--)
+                {
+                    U[i, j] = alpha[j + 1] * U[i, j + 1] + beta[j + 1];
+                }
+            }
+
+            curT += tau;
+            conditionT();
+        }
+
+        static T[,] Copy<T>(T[,] array)
+        {
+            T[,] newArray = new T[array.GetLength(0), array.GetLength(1)];
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                    newArray[i, j] = array[i, j];
+            return newArray;
+        }
+
+    }
+
+}
